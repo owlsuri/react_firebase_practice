aa413436337c3bfb130e62af6a8498f854b8e356
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1 <acwell94@naver.com>
2022-07-10 00:27:55 +0900

    초기설정

---
+++ src/Firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvIpKsvCKcZCdhlWX0JEUmlW_2OkfCnQY",
+  authDomain: "reacttoyproject-c5488.firebaseapp.com",
+  projectId: "reacttoyproject-c5488",
+  storageBucket: "reacttoyproject-c5488.appspot.com",
+  messagingSenderId: "49031757657",
+  appId: "1:49031757657:web:bd90e5517af4eb57801648",
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
