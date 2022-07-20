07a337928ee8acc061b30845010c767c58aa722f
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1 <acwell94@naver.com>
2022-07-20 12:15:12 +0900

    회원가입 페이지 CSS 완성

--- src/components/commons/inputs/input.tsx
+++ src/components/commons/inputs/input.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const InputStyled = styled.input`
+  background-color: #e5e5e5;
+  border: none;
+  border-radius: 8px;
+  outline: none;
   width: 100%;
+  padding: 26px 0px 26px 25px;
+  color: #000000;
 `;
 
 interface IProps {
--- src/components/pages/routes/SignUpPage.tsx
+++ src/components/pages/routes/SignUpPage.tsx
@@ -6,32 +6,30 @@ import ErrorMsg from "../../commons/errorMsg/errorMsg";
 
 function SignUpPage() {
   return (
-    <div>
-      <S.Main>
-        <S.Title>SIGN UP</S.Title>
-        <form>
-          <S.InputWrapper>
-            <Input type="email" placeholder="이메일을 입력해주세요" />
-            <ErrorMsg contents="올바른 이메일을 입력해주세요" />
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <Input type="password" placeholder="비밀번호를 입력해주세요" />
-            <ErrorMsg contents="올바른 이메일을 입력해주세요" />
-          </S.InputWrapper>
-          <S.InputWrapper>
-            <Input type="text" placeholder="닉네임을 입력해주세요" />
-            <ErrorMsg contents="올바른 이메일을 입력해주세요" />
-          </S.InputWrapper>
-          <S.SignInWrapper>
-            <div>이미 회원이신가요?</div>
-            <div>로그인하러가기</div>
-          </S.SignInWrapper>
-          <div>
-            <SignButton contents="회원가입" />
-          </div>
-        </form>
-      </S.Main>
-    </div>
+    <S.Main>
+      <S.Title>SIGN UP</S.Title>
+      <S.Form>
+        <S.InputWrapper>
+          <Input type="email" placeholder="이메일을 입력해주세요" />
+          <ErrorMsg contents="올바른 이메일을 입력해주세요" />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <Input type="password" placeholder="비밀번호를 입력해주세요" />
+          <ErrorMsg contents="올바른 이메일을 입력해주세요" />
+        </S.InputWrapper>
+        <S.InputWrapper>
+          <Input type="text" placeholder="닉네임을 입력해주세요" />
+          <ErrorMsg contents="올바른 이메일을 입력해주세요" />
+        </S.InputWrapper>
+        <S.SignInWrapper>
+          <S.SignInInfo>이미 회원이신가요?</S.SignInInfo>
+          <S.SignInLink>로그인하러가기</S.SignInLink>
+        </S.SignInWrapper>
+        <S.SignUpBtnWrapper>
+          <SignButton contents="회원가입" />
+        </S.SignUpBtnWrapper>
+      </S.Form>
+    </S.Main>
   );
 }
 
