e7ea651ccec3b9ae696edd86f7b6b52b4721627c
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1 <acwell94@naver.com>
2022-07-25 15:55:12 +0900

    header 추가(Link, CSS)

---
+++ src/components/pages/Layout/header/header.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { colorPick } from "../../../commons/color/colorPick";
+
+interface IProps {
+  isMenuOpen: boolean;
+}
+
+const Header = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  width: 300px;
+  height: 100px;
+`;
+
+const HamburgerBtn = styled.div`
+  width: 35px;
+  height: 35px;
+  cursor: pointer;
+`;
+
+const Menu = styled.div`
+  display: ${(props: IProps) => (props.isMenuOpen ? "flex" : "none")};
+  flex-direction: column;
+  position: absolute;
+  top: 100px;
+  right: 14%;
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+`;
+
+const DropDownMenuFirst = styled.div`
+  text-align: center;
+  width: 100%;
+  padding: 10px 65px;
+  font-weight: 500;
+  border-radius: 8px 8px 0px 0px;
+  :hover {
+    background-color: ${colorPick.purple};
+    color: white;
+  }
+`;
+
+const DropDownMenuMid = styled(DropDownMenuFirst)`
+  border-radius: 0;
+`;
+const DropDownMenuLast = styled(DropDownMenuFirst)`
+  border-radius: 0px 0px 8px 8px;
+`;
+
+export default function HeaderPage() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const onClickMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
+  console.log(isMenuOpen);
+  return (
+    <Header>
+      <div></div>
+      <Link to={`/mypage/:id`}>
+        <Logo
+          style={{
+            backgroundImage: `url(/images/logo.png)`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}
+        ></Logo>
+      </Link>
+      <HamburgerBtn
+        onClick={onClickMenu}
+        style={{
+          backgroundImage: `url(/images/more.png)`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
+      ></HamburgerBtn>
+      <Menu isMenuOpen={isMenuOpen}>
+        <DropDownMenuFirst>LIST</DropDownMenuFirst>
+        <DropDownMenuMid>MYPAGE</DropDownMenuMid>
+        <DropDownMenuLast>LOGOUT</DropDownMenuLast>
+      </Menu>
+    </Header>
+  );
+}
--- src/components/pages/routes/ListPage.tsx
+++ src/components/pages/routes/ListPage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import HeaderPage from "../Layout/header/header";
 
 function ListPage() {
-  return <div>여기는 리스트페이지 입니다.</div>;
+  return (
+    <div>
+      <HeaderPage />
+      여기는 리스트페이지 입니다.
+    </div>
+  );
 }
 
 export default ListPage;
