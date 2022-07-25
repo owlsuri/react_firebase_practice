8334325b994deb8b5a709e11f12f717ce6d438a9
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1 <acwell94@naver.com>
2022-07-25 16:44:30 +0900

    Nav 추가(CSS완성 및 Link추가)

---
+++ src/components/pages/Layout/nav/nav.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { colorPick } from "../../../commons/color/colorPick";
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0px 55px 0px;
+`;
+
+const NavSection = styled.div`
+  display: flex;
+  border-radius: 100px;
+
+  :hover {
+    #write {
+      display: block;
+      #intro {
+        display: none;
+      }
+    }
+  }
+`;
+
+const IntroWrapper = styled.div`
+  text-align: center;
+  width: 500px;
+  background-color: ${colorPick.gray};
+  color: white;
+  padding: 10px 0px;
+  border-radius: 100px;
+`;
+
+const MoveToWriteWrapper = styled(Link)`
+  display: none;
+  text-align: center;
+  width: 500px;
+  position: absolute;
+  background-color: ${colorPick.purple};
+  color: white;
+  padding: 10px 0px;
+  border-radius: 100px;
+  cursor: pointer;
+  text-decoration: none;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+export default function NavPage() {
+  return (
+    <Nav>
+      <NavSection>
+        <IntroWrapper id="intro">민영님, 어떤 하루를 보내셨나요?</IntroWrapper>
+        <MoveToWriteWrapper to={"/write"} id="write">
+          3줄일기 쓰러가기
+        </MoveToWriteWrapper>
+      </NavSection>
+    </Nav>
+  );
+}
