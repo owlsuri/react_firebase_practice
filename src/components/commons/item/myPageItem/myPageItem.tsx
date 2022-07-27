24c130e5714e0f2efbadd04c198111340973b054
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1 <acwell94@naver.com>
2022-07-27 17:22:24 +0900

    WrietPage Mood 작성 추가 및 MyPage 상단 CSS완성

--- src/components/commons/item/myPageItem/myPageItem.tsx
+++ src/components/commons/item/myPageItem/myPageItem.tsx
@@ -1,10 +1,48 @@
 import React from "react";
+import { emotionImg } from "../../data/EmotionData";
+import { relationImg } from "../../data/relationData";
+import { weatherImg } from "../../data/weatherData";
 import * as S from "./myPageItem.styles";
 
 export default function MyPageItem() {
   return (
     <S.Main>
-      <div>이것은 마이페이지 아이템입니다.</div>
+      <S.MainWrapper>
+        <S.MainLeft>
+          <S.MainLeftTitle>3다일기</S.MainLeftTitle>
+          <S.MainLeftDailyCount>215</S.MainLeftDailyCount>
+        </S.MainLeft>
+        <S.MainRight>
+          <S.MainRightSortInfo>
+            날씨별 기분별 누구와 함께 했는지로 추억을 찾아보세요!
+          </S.MainRightSortInfo>
+          <S.SortImg src="/images/sort-down.png" />
+        </S.MainRight>
+      </S.MainWrapper>
+      <S.BottomWrapper>
+        <S.SortLeftWrapper>
+          <S.SortInfo>기분</S.SortInfo>
+          <S.SortInfoMiddle>날씨</S.SortInfoMiddle>
+          <S.SortInfo>누구와</S.SortInfo>
+        </S.SortLeftWrapper>
+        <S.SortRightWrapper>
+          <S.SortImgWrapper>
+            {emotionImg.map((el) => (
+              <S.ForSortImg key={el.key} src={el.img} />
+            ))}
+          </S.SortImgWrapper>
+          <S.SortImgMiddleWrapper>
+            {weatherImg.map((el) => (
+              <S.ForSortImg key={el.key} src={el.img} />
+            ))}
+          </S.SortImgMiddleWrapper>
+          <S.SortImgWrapper>
+            {relationImg.map((el) => (
+              <S.ForSortImg key={el.key} src={el.img} />
+            ))}
+          </S.SortImgWrapper>
+        </S.SortRightWrapper>
+      </S.BottomWrapper>
     </S.Main>
   );
 }
